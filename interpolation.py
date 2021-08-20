de0b6c4faa7a4f735e2ee7656276dcf59260ad77
Zhenhuann Sun <sunsteven11@gmail.com>
2021-08-20 14:51:58 -0400

    Added bicubic interpolation in interpolation class

--- interpolation.py
+++ interpolation.py
@@ -1,15 +1,134 @@
 import numpy as np
 import cv2
 
-image = cv2.imread('./Image/img_example_lr.png')
-cv2.imshow('image', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.waitKey(1)
-
 class interpolation():
-    def __init__(self, interpolation="bicubic", scale=1):
-        self.interpolation = interpolation
+    def __init__(self, image_path, scale=1):
+        # load image to numpy array
+        self.image = cv2.imread(image_path)
+
+        # size of original image
+        self.image_width = self.image.shape[0]
+        self.image_height = self.image.shape[1]
+
+        # size of output image after rescaling
+        self.resized_image_width = self.image_width * scale
+        self.resized_image_height = self.image_height * scale
+
+        # the amount of scale
         self.scale = scale
 
-    
+    # resize image using bicubic interpolation
+    def bicubic(self):
+        # empty list to store each resized rgb channel
+        image_rgb = []
+
+        B = np.array([[-1, 1, -1, 1], [0, 0, 0, 1], [1, 1, 1, 1], [8, 4, 2, 1]])
+        B_inv = np.linalg.inv(B)
+        B_inv_T = B_inv.T 
+
+        # perform bicubic interpolation for each color channel
+        for i in range(3):
+            image_channel = self.image[:, :, i]
+
+            # pre-allcoate space for output image in current color channel
+            # make sure the data type is unsigned integer 8 since only uint8 data type are accepted in image file
+            image_channel_output = np.zeros((self.resized_image_width, self.resized_image_height), dtype=np.uint8)
+
+            # pad original image channel to make sure every pixel in the original image can find 16 data points to
+            # perform bicubic interpolation
+            # pad row
+            top_row = image_channel[0, :]
+            bottom_row = image_channel[-1, :]
+            image_row_padding = np.vstack((top_row, image_channel))
+            image_row_padding = np.vstack((image_row_padding, bottom_row))
+            image_row_padding = np.vstack((image_row_padding, bottom_row))
+
+            # pad column
+            leftmost_column = image_row_padding[:, 0]
+            rightmost_column = image_row_padding[:, -1]
+            image_padding = np.c_[leftmost_column, image_row_padding, rightmost_column, rightmost_column]
+
+            # preallocate space for F matrix
+            # F stores f matrix for each pixel in the original image matrix (using image_padding)
+            # each f is a 4 by 4 matrix that stores 16 pixel values for bicubic interpolation
+            F = np.zeros((self.image_width, self.image_height, 4, 4))
+
+            for x in range(self.image_width):
+                x_padding = x + 1 # reposition x index since we are using image martix after padding
+                for y in range(self.image_height):
+                    y_padding = y + 1 # reposition y index since we are using image matrix after padding
+                    
+                    # formalize f matrix
+                    f = np.array([[image_padding[x_padding-1][y_padding-1], image_padding[x_padding-1][y_padding], image_padding[x_padding-1][y_padding+1], image_padding[x_padding-1][y_padding+2]],
+                                  [image_padding[x_padding][y_padding-1],   image_padding[x_padding][y_padding],   image_padding[x_padding][y_padding+1],   image_padding[x_padding][y_padding+2]],
+                                  [image_padding[x_padding+1][y_padding-1], image_padding[x_padding+1][y_padding], image_padding[x_padding+1][y_padding+1], image_padding[x_padding+1][y_padding+2]],
+                                  [image_padding[x_padding+2][y_padding-1], image_padding[x_padding+2][y_padding], image_padding[x_padding+2][y_padding+1], image_padding[x_padding+2][y_padding+2]]])
+
+                    # store f in F
+                    F[x][y] = f
+
+            # interpolate image pixel by pixel
+            for x in range(self.resized_image_width):
+                # compute the corresponding x position in the original image
+                x_ori = (x / self.resized_image_width) * self.image_width
+
+                # the amount of interpolation in x axis(eg: x_ori=1.5, then x_interp=0.5, 0.5 from x=1)
+                x_interp = x_ori - np.floor(x_ori)
+
+                # integer part of x_ori (used to index F matrix created above)
+                x_int = int(np.floor(x_ori))
+
+                for y in range(self.resized_image_height):
+                    # compute the corresponding y position in the original image
+                    y_ori = (y / self.resized_image_height) * self.image_height
+
+                    # the amount of interpolation in y axis
+                    y_interp = y_ori - np.floor(y_ori)
+
+                    # integer part of y_ori (used to index F matrix created above)
+                    y_int = int(np.floor(y_ori))
+
+                    # if the program find that the interpolated point can be found in the original image matrix
+                    # eg: (x_ori, y_ori) = (x, y) = (0, 0)
+                    # then there is no need to perform bicubic interpolation for that point
+                    # we can simply copy the pixel value of that point from original image to scaled image
+                    if x_interp==0.0 and y_interp==0.0:
+                        # there should always be (orginal image width * original image height) times we can copy pixel value like this
+                        # for three rgb channel we can enter into this if statement for 3 * (original image width * origianl image height) times
+                        image_channel_output[x][y] = image_channel[int(x_ori)][int(y_ori)]
+                    else:
+                        # interpolate value in x direction (row vector)
+                        X = np.expand_dims(np.array([x_interp**3, x_interp**2, x_interp**1, x_interp**0]), axis=0)
+                        # interpolate value in y direction (column vector)
+                        Y = np.expand_dims(np.array([y_interp**3, y_interp**2, y_interp**1, y_interp**0]), axis=1)
+
+                        # f matrix at this point
+                        F_interp = F[x_int][y_int]
+
+                        # bicubic interpolation
+                        interpolated_value = X.dot(B_inv).dot(F_interp).dot(B_inv_T).dot(Y)
+
+                        # after bicubic interpolation between adjacent pixels, floating point value will be retured
+                        # instead of usigned integer 8. We need to clamp the value between 0 and 255 in order to show
+                        # image matrix as an image
+                        if interpolated_value < 0:
+                            interpolated_value = 0
+                        elif interpolated_value > 255:
+                            interpolated_value=255
+
+                        image_channel_output[x][y] = interpolated_value
+            
+            # insert resized rgb channel image matrix into a list
+            image_rgb.append(image_channel_output)
+        
+        # merge 3 image color channel into a image
+        image_rgb_output = cv2.merge((image_rgb[0], image_rgb[1], image_rgb[2]))
+
+        return image_rgb_output
+
+
+interp = interpolation(image_path="./Image/img_example_lr.png", scale=2)
+resized_image = interp.bicubic()
+cv2.imshow("resized image", resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
