2ce53dcbe746643ba28de6e1caa5dbf66138a3c1
Zhenhuann Sun <sunsteven11@gmail.com>
2021-08-21 21:59:58 -0400

    Updated comments in main.py

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ resized_image_bicubic = interp.bicubic()        # bicubic interpolation
 # stack image matrix horizontally in order to show 3 images in one window
 image = np.hstack((resized_image_nn, resized_image_bilinear, resized_image_bicubic))
 
+
+# show resized image
 cv2.imshow("Nearest Neighbour Interpolation", resized_image_nn)
 cv2.imshow("Bilinear Interpolation", resized_image_bilinear)
 cv2.imshow("Bicubic Interpolation", resized_image_bicubic)
+# compare different interpolation method
 cv2.imshow("Interpolation comparision", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
